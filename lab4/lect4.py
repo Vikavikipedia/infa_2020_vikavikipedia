6c69e0f638f6abb36c82e4ff41940073a6be0309
Kiseleva Viktoriya <viktoriya.kiseleva@phystech.edu>
2022-06-13 15:49:31 +0300

    created a function which draws house. Created a prototype of the function which draws house and its interface. the interface was documented

---
+++ lab4/lect4.py
@@ -0,0 +1,18 @@
+
+def draw_house(x, y, width, height):
+    """
+    Function draws a house with width and height from reference point (x, y),
+    which is in the bottom middle of the house basement
+    :param x: coordinate of the middle of the house
+    :param y: coordinate of the bottom of the basement
+    :param width: full width of the house with roof
+    :param height: full height of the house with roof and basement
+    :return: None
+    """
+    print('Draw house', x, y, width, height)
+    pass    # do nothind
+
+x, y = 100, 100
+width, height = 200, 200
+
+draw_house(x, y, width, height)
